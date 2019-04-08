1fa5918f0b5afeed65d370a4a8f54a303da845c0
Fabian Beckmann <fabianb@moduleworks.com>
2019-04-08 20:04:59 +0200

    avoid some more case-insensitive compares

--- Source/cmSystemTools.cxx
+++ Source/cmSystemTools.cxx
@@ -2224,7 +2224,6 @@ void cmSystemTools::FindCMakeResources(const char* argv0)
     // ???
   }
 #endif
-  exe_dir = cmSystemTools::GetActualCaseForPath(exe_dir);
   cmSystemToolsCMakeCommand = exe_dir;
   cmSystemToolsCMakeCommand += "/cmake";
   cmSystemToolsCMakeCommand += cmSystemTools::GetExecutableExtension();
